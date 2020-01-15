00cb89dec81bc36d30b26e638fec0b381148c501
vagbergca <59940543+vagbergca@users.noreply.github.com>
2020-01-15 18:06:27 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,23 @@
+/*********************************************************************************
+* Hello
+**********************************************************************************
+* Carl Adrian Vagberg
+* January 15, 2020
+* CMSC 255-901
+*********************************************************************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Carl Adrian Vagberg");
+		System.out.println("Lab 1");
+		System.out.println("Installing all necessary software");
+		System.out.println("January 15, 2020");
+		System.out.println("CMSC 255 901");
+		System.out.println();
+		}
+	}
+	
+	
+		
